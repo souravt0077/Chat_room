2ceff39bb3b05178c486359fe89055ec37590539
sourav <souravt0077@gmail.com>
2023-01-09 21:38:35 +0530

    room CRUD

--- chatroom/base/urls.py
+++ chatroom/base/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('rooms/<str:pk>/',views.rooms,name='rooms'),
+    path('createRoom/',views.createRoom,name='createroom'),
+    path('updateRoom/<str:pk>/',views.updateRoom,name='updateroom'),
+    path('deleteRoom/<str:pk>/',views.deleteRoom,name='deleteroom'),
 ]
--- chatroom/base/views.py
+++ chatroom/base/views.py
@@ -1,8 +1,44 @@
-from django.shortcuts import render
-from user.models import User
+from django.shortcuts import render,redirect
+# from user.models import User
+from.models import Room
+from .form import roomForm
 # Create your views here.
 
 def home(request):
-    users=User.objects.all()
-    context={'users':users}
+    rooms=Room.objects.all()
+    context={'rooms':rooms}
     return render(request,'home.html',context)
+
+def rooms(request,pk):
+    rooms=Room.objects.get(id=pk)
+    context={'rooms':rooms}
+    return render(request,'room.html',context)
+
+def createRoom(request):
+    form=roomForm()
+    if request.method == 'POST':
+        form=roomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={'form':form}
+    return render(request,'room_form.html',context)
+
+def updateRoom(request,pk):
+    room=Room.objects.get(id=pk)
+    form=roomForm(instance=room)
+    if request.method == 'POST':
+        form=roomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('rooms',room.id)
+    context={'form':form}
+    return render(request,'room_form.html',context)
+
+def deleteRoom(request,pk):
+    room=Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    context={'obj':room}
+    return render(request,'deleteRoom.html',context)
